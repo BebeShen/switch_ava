4ec2fc8081c15d8ca34c186e1dcd4df3e5de1c4d
Bebeshen <obebesheno@gmail.com>
2024-06-14 16:56:27 +0800

    [Feat] Complete script & Add to cronjob

--- switch_conn_test.py
+++ switch_conn_test.py
@@ -1,5 +1,6 @@
 import paramiko
 import requests
+import datetime
 import json
 import os
 from dotenv import load_dotenv
@@ -27,15 +28,15 @@ def check_ssh(ip, user, psw):
             stdin, stdout, stderr = client.exec_command("curl http://[fe80::ff:fe00:1%usb0]:8080/api/sys/bmc")
             result = stdout.readlines()[0]
             print(json.dumps(json.loads(result), indent=4))
-        return True
+        return True, None
     except Exception as e:
         print("[-] Connection Fail!")
         print("========================")
         print(e)
         print("========================")
-        return False
+        return False, e
 
-def line_notify():
+def line_notify(msg):
     url = 'https://notify-api.line.me/api/notify'
     token = os.getenv('SECRET')
     # 設定權杖
@@ -43,29 +44,39 @@ def line_notify():
         'Authorization': 'Bearer ' + token    
     }
     # 發送的訊息
+    message = '\n'.join(msg)
+    print(message)
     data = {
-        'message':'測試一下！\n換行'     
+        'message':'\n'+message     
     }
     # 使用 POST 方法
     data = requests.post(url, headers=headers, data=data)   
 
 def main():
+    message = []
+
+    now = datetime.datetime.now().strftime("%Y/%m/%d %H:%M")
+    message.append(now)
     # switch
-    check_ssh("10.30.3.68", user="root", psw=os.getenv('PSW_OLD'))
-    check_ssh("10.30.3.69", user="root", psw=os.getenv('PSW_NEW'))
-    check_ssh("10.30.3.70", user="root", psw=os.getenv('PSW_NEW'))
-    check_ssh("10.30.3.71", user="root", psw=os.getenv('PSW_NEW'))
-    check_ssh("10.30.3.72", user="root", psw=os.getenv('PSW_NEW'))
-    check_ssh("10.30.3.73", user="root", psw=os.getenv('PSW_NEW'))
+    for s in bmc_check_list:
+        if check_ssh(s, user="root", psw=os.getenv('PSW_NEW')):
+            message.append("[+] Switch[{}] is UP!".format(s))
+        else:
+            message.append("[-] Switch[{}] is DOWN!".format(s))
+
     # 73 bmc
     if check_ssh("10.30.3.76", user="root", psw=os.getenv('PSW_BMC')) :
-        print("73 bmc UP")
+        message.append("[+] BMC(73) 10.30.3.76 is UP!")
     else :
-        print("73 bmc DOWN")
+        message.append("[-] BMC(73) 10.30.3.76 is DOWN!")
     # server
-    check_ssh("10.30.3.75", user="root", psw=os.getenv('PSW_SERVER'))
+    if check_ssh("10.30.3.75", user="root", psw=os.getenv('PSW_SERVER')) :
+        message.append("[+] Server[{}] is UP!".format(s))
+    else :
+        message.append("[-] Server[{}] is DOWN!".format(s))
+    
+    line_notify(message)
 
 
 if __name__ == '__main__':
-    line_notify()
-    # main()
+    main()
